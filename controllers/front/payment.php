9f620ad53dc9d7776920c5373f02b9fbc4a581c9
MathiasReker <mathias@reker.dk>
2018-12-21 00:20:06 +0100

    Indentatio type

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -46,14 +46,14 @@ class BankwirePaymentModuleFrontController extends ModuleFrontController
         }
 
         $this->context->smarty->assign([
-		    'nbProducts' => $cart->nbProducts(),
-		    'cust_currency' => $cart->id_currency,
-		    'currencies' => $this->module->getCurrency((int) $cart->id_currency),
-		    'total' => $cart->getOrderTotal(true, Cart::BOTH),
-		    'this_path' => $this->module->getPathUri(),
-		    'this_path_bw' => $this->module->getPathUri(),
-		    'this_path_ssl' => Tools::getShopDomainSsl(true, true) . __PS_BASE_URI__ . 'modules/' . $this->module->name . '/'
-		]);
+            'nbProducts' => $cart->nbProducts(),
+            'cust_currency' => $cart->id_currency,
+            'currencies' => $this->module->getCurrency((int) $cart->id_currency),
+            'total' => $cart->getOrderTotal(true, Cart::BOTH),
+            'this_path' => $this->module->getPathUri(),
+            'this_path_bw' => $this->module->getPathUri(),
+            'this_path_ssl' => Tools::getShopDomainSsl(true, true) . __PS_BASE_URI__ . 'modules/' . $this->module->name . '/'
+        ]);
 
         $this->setTemplate('payment_execution.tpl');
     }
